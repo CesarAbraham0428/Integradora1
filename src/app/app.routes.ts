24914c5ea41f4c9ce7030fef6a9d17c996cb8c80
CesarAbraham0428 <CesarAbraham0428@gmail.com>
2024-06-11 20:18:07 -0600

    Agregando Parte de la  Pagina Principal desde Cesar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
+import { InicioComponent } from './pages/inicio/inicio.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: InicioComponent
+
+    }
+];
